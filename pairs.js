be429264dee59172e002a3bcb58665734e68c2e5
zkouskystranek <269227743+zkouskystranek@users.noreply.github.com>
2026-04-01 13:15:14 +0200

    Add files via upload

---
+++ pairs.js
@@ -0,0 +1,20 @@
+// pairs.js
+// Konfigurace datasetu a seznam všech dostupných obrázků
+// - draw: kolik párů se má losovat do pexesa (default si můžeš měnit)
+window.DATASET = {
+  draw: 10,   // kolik položek (párů) se náhodně vybere do pexesa
+  items: [
+ { file: "obr1.jpg",  name: "štěpa z depa" },
+    { file: "obr2.jpg",  name: "ddop" },
+    { file: "obr3.jpg",  name: "kikiriki" },
+    { file: "obr4.jpg",  name: "dorieta" },
+    { file: "obr5.jpg",  name: "laurinka posranka" },
+    { file: "obr6.jpg",  name: "kamilo lepidlo" },
+    { file: "obr7.jpg",  name: "pidza z vidca" },
+    { file: "obr8.jpg",  name: "propan butan" },
+    { file: "obr9.jpg",  name: "rakosnicek" },
+    { file: "obr10.jpg", name: "brumik"},
+    { file: "obr11.jpg", name: "Martinka" },
+    { file: "obr12.jpg", name: "Matěj" },
+  ]
+};
